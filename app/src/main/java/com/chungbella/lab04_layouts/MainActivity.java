9a3393d91d0f62cd3a0792ee0bf501204488d5ed
C0nnwaer <webellachung@gmail.com>
2020-02-25 10:10:18 -0500

    create layout 5

--- app/src/main/java/com/chungbella/lab04_layouts/MainActivity.java
+++ app/src/main/java/com/chungbella/lab04_layouts/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // setContentView(R.layout.layout_1);
 //        setContentView(R.layout.layout_2);
-        setContentView(R.layout.layout_3);
+//        setContentView(R.layout.layout_3);
+        setContentView(R.layout.layout_4);
     }
 }
