51355edab7330cba9bd9879f59d181bde76ae0a8
C0nnwaer <webellachung@gmail.com>
2020-02-25 09:31:08 -0500

    third layout created

--- app/src/main/java/com/chungbella/lab04_layouts/MainActivity.java
+++ app/src/main/java/com/chungbella/lab04_layouts/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // setContentView(R.layout.layout_1);
-        setContentView(R.layout.layout_2);
+//        setContentView(R.layout.layout_2);
+        setContentView(R.layout.layout_3);
     }
 }
